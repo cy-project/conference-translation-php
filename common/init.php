e12268d1212a2d9d4fe21a8c5500ad2459661b7a
ali <s11113151@stu.edu.tw>
2013-08-17 17:02:14 +0800

    fix typo

--- common/init.php
+++ common/init.php
@@ -2,6 +2,6 @@
 
 require(__DIR__ .'/password.php');
 require(__DIR__ .'/config.php');
-require(__DIR__ .'/database.php');
+require(__DIR__ .'/Database.php');
 require(__DIR__ .'/functions.php');
 require(__DIR__ .'/defsMsg.php');
