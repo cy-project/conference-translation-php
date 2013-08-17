bdb9bcdc55deb686175825ff92ece56700bff477
ali <s11113151@stu.edu.tw>
2013-08-17 18:41:53 +0800

    修正縮排

--- common/example-config.php
+++ common/example-config.php
@@ -3,13 +3,13 @@
 /**
  *  Database Params
  */
-define('DB_NAME'     , '');
-define('DB_TYPE'     , '');
-define('DB_USERNAME' , getUsername());
-define('DB_PASSWORD' , getPassword());
-define('DB_ENCODING'   , '');
-define('DB_HOST'     , '');
-define('DB_SALT'     , '');
+define('DB_NAME'        , '');
+define('DB_TYPE'        , '');
+define('DB_USERNAME'    , getUsername());
+define('DB_PASSWORD'    , getPassword());
+define('DB_ENCODING'    , '');
+define('DB_HOST'        , '');
+define('DB_SALT'        , '');
 
 /**
  * Database Tables
