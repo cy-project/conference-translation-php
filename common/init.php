35a39740006955c96f33eb27d4149fe381937cf9
ali <s11113151@stu.edu.tw>
2013-08-17 11:47:16 +0800

    require defsMsg

--- common/init.php
+++ common/init.php
@@ -4,3 +4,4 @@ require_once(dirname(__FILE__) .'/password.php');
 require_once(dirname(__FILE__) .'/config.php');
 require_once(dirname(__FILE__) .'/db.php');
 require_once(dirname(__FILE__) .'/functions.php');
+require_once(dirname(__FILE__) .'/defsMsg.php');
